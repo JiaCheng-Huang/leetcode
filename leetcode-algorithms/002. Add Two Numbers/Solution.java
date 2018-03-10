63f11ccaa1d80c133492b78776c23e09b09d7539
JiaCheng-Huang <Dylan.JiaCheng.Huang@outlook.com>
2018-03-10 23:04:07 +0800

    AC

---
+++ leetcode-algorithms/002. Add Two Numbers/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        int carry = 0;
+        ListNode p1 = l1, p2 = l2, r = result;
+        while (p1 != null || p2 != null) {
+            int x = (p1 != null) ? p1.val : 0;
+            int y = (p2 != null) ? p2.val : 0;
+            int tmp = x + y + carry;
+            r.next = new ListNode(tmp % 10);
+            carry = tmp / 10;
+            if (p1 != null) {
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                p2 = p2.next;
+            }
+            r = r.next;
+        }
+        if (carry > 0) {
+            r.next = new ListNode(carry);
+        }
+        return result.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
